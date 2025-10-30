2cf2ee6e8bc50ee75ee6bbaf407827b4fbb9ffe7
VishnuAM01 <vishnu.arakkal.13@gmail.com>
2025-10-30 11:02:33 +0530

    Q3-Todo List

---
+++ Q3-Todo List/HomePage.jsx
@@ -0,0 +1,36 @@
+import React,{useState} from 'react';
+
+function Home(){
+    const [isChecked, setIsChecked] = useState(false);
+    const [todos, setTodos]=useState([]);
+    const [inputValue, setInputValue]=useState([]);
+    const addTask = ()=>{
+        const newToDo={
+            id: Date.now(),
+            text: inputValue
+        };
+        setTodos((prevTodos)=>[...todos,newItem]);
+        setInputValue('');
+    }
+    return(
+        <div>
+            <h1>Todo List</h1>
+            <label>
+                <input type="checkbox"
+                checked={isChecked}
+                onChange={()=>setIsChecked(!isChecked)} 
+                /> 
+                <input type="text" value={inputValue}
+                onChange={(e)=>setInputValue(e.target.value)}
+                placeholder='Enter a new task'
+                />
+            </label>
+            <button onClick={addTask}>Add new task</button>
+
+        </div>
+        
+
+
+    );
+}
+export default Home
---
+++ Q3-Todo List/README.md
@@ -0,0 +1,6 @@
+# ToDo List
+
+First we create a check box.
+Then we are adding items to the List using a text box
+Add a button to create new tasks
+
